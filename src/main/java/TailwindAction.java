ccff154b010f4407d2caa312e046c68355c73c87
JL <jay.lindsley@gmail.com>
2020-02-25 11:11:05 -0800

    Plugin, File Finding
    Updating plugin version. Updating to detect when no file is currently open.

--- src/main/java/TailwindAction.java
+++ src/main/java/TailwindAction.java
@@ -3,6 +3,7 @@ import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 
 public class TailwindAction extends AnAction {
     TailwindParser parser = new TailwindParser();
@@ -12,6 +13,10 @@ public class TailwindAction extends AnAction {
     }
 
     public void actionPerformed(AnActionEvent event) {
+        VirtualFile currentFile = event.getData(CommonDataKeys.VIRTUAL_FILE);
+        if(currentFile == null) {
+            return;
+        }
         // Get all the required data from data keys
         final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
         final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
