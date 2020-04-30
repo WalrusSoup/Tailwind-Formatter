aa7c4c52f8c09c0600999de67db6c7adb21a05f6
JL <jl@myoutdesk.com>
2020-04-30 11:52:05 -0700

    Adding Auto Import button
    This update adds a button to import defaults into the Custom Class Sort Order. This should allow users to properly modify it without having to track down the original order.

--- src/main/java/TailwindConfigurable.java
+++ src/main/java/TailwindConfigurable.java
@@ -5,14 +5,25 @@ import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class TailwindConfigurable implements Configurable, Configurable.NoScroll {
     private JPanel rootPanel;
     private JTextArea customClassOrderTextArea;
+    private JLabel AutoImport;
     private final TailwindSettings settings;
 
     public TailwindConfigurable(Project project) {
         settings = TailwindSettings.getInstance(project);
+        AutoImport.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+                TailwindUtility utility = new TailwindUtility();
+                customClassOrderTextArea.setText(String.join("\n", utility.classOrder));
+            }
+        });
     }
 
     @Nls(capitalization = Nls.Capitalization.Title)
