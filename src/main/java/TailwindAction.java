9da4fd629bb2d71328a7511d6315146e7d39a69b
JL <jay.lindsley@gmail.com>
2020-04-30 13:15:46 -0700

    Manually Merging
    Manually merging the feature-apply from #14 that failed.

--- src/main/java/TailwindAction.java
+++ src/main/java/TailwindAction.java
@@ -5,34 +5,25 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
-import java.util.List;
-
 public class TailwindAction extends AnAction {
+    TailwindParser parser = new TailwindParser();
+
     public TailwindAction() {
         super("Run Tailwind Formatter");
     }
 
     public void actionPerformed(AnActionEvent event) {
         VirtualFile currentFile = event.getData(CommonDataKeys.VIRTUAL_FILE);
-        if (currentFile == null) {
+        if(currentFile == null) {
             return;
         }
         // Get all the required data from data keys
         final Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
         final Project project = event.getRequiredData(CommonDataKeys.PROJECT);
         final Document document = editor.getDocument();
-        final TailwindSettings settings = TailwindSettings.getInstance(project);
-
-        final TailwindParser parser = new TailwindParser(new TailwindSorter(getClassOrder(settings)));
         String body = parser.processBody(document.getText());
         WriteCommandAction.runWriteCommandAction(project, () -> {
             document.setText(body);
         });
-    }
-
-    private List<String> getClassOrder(TailwindSettings settings) {
-        final List<String> customClassOrder = settings.getCustomClassOrderList();
-
-        return !customClassOrder.isEmpty() ? customClassOrder : new TailwindUtility().classOrder;
     }
 }
--- src/main/java/TailwindParser.java
+++ src/main/java/TailwindParser.java
@@ -5,30 +5,50 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TailwindParser {
-    private final String regex = "\\bclass(?:Name)*\\s*=\\s*([\\\\\"\\']([_a-zA-Z0-9\\s\\-\\:\\/]+)[\\\\\"\\'])";
-    private final TailwindSorter sorter;
+    private final String classNamesRegex = "[_a-zA-Z0-9\\s\\-\\:\\/]+";
+    private final String regex = "\\bclass(?:Name)*\\s*=\\s*(?:[\\\"\\'](?<classList1>" + classNamesRegex + ")[\\\"\\'])|@apply (?<classList2>" + classNamesRegex + ");";
+    private TailwindSorter tailwindSorter = new TailwindSorter();
 
-    public TailwindParser(TailwindSorter sorter) {
-        this.sorter = sorter;
-    }
-
-    public String processBody(String body)
-    {
+    public String processBody(String body) {
         Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
         Matcher matcher = pattern.matcher(body);
 
         while (matcher.find()) {
-            // Grab the original entry class="container mx-auto bg-black"...
-            String targetReplacement = matcher.group(1);
+            final boolean isApplyStatement;
+            final String originalClassList;
             // Grab the inner contents of the class list for deduplication and arranging
-            String originalClassList = matcher.group(2);
-            List<String> currentClasses = Arrays.asList(originalClassList.split(" "));
+            if (matcher.group("classList1") != null && !matcher.group("classList1").isEmpty()) {
+                isApplyStatement = false;
+                originalClassList = matcher.group("classList1");
+            } else {
+                isApplyStatement = true;
+                originalClassList = matcher.group("classList2");
+            }
+
+            if (originalClassList.contains("\n")) {
+                // multiline class lists are currently not supported
+                continue;
+            }
+
+            final List<String> currentClasses = Arrays.asList(originalClassList.trim().split(" +"));
             // Sort the list of classes
-            currentClasses.sort(sorter);
+            currentClasses.sort(tailwindSorter);
             // Create a linked hash set to remove duplicates
-            LinkedHashSet<String> currentClassesWithoutDuplicates = new LinkedHashSet<String>(currentClasses);
-            body = body.replace(targetReplacement, "\"" + String.join(" ", currentClassesWithoutDuplicates) + "\"");
+            final LinkedHashSet<String> currentClassesWithoutDuplicates = new LinkedHashSet<String>(currentClasses);
+            body = body.replace(
+                    encloseClassList(originalClassList, isApplyStatement),
+                    encloseClassList(String.join(" ", currentClassesWithoutDuplicates), isApplyStatement)
+            );
         }
+
         return body;
+    }
+
+    private String encloseClassList(String classList, boolean isApplyStatement) {
+        if (isApplyStatement) {
+            return "@apply " + classList + ";";
+        }
+
+        return "\"" + classList + "\"";
     }
 }
