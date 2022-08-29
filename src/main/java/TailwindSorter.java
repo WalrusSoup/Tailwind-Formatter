f416f2b90656ee02384191d9e171ffd4cb90d4ee
JL <jay.lindsley@gmail.com>
2022-08-29 14:17:40 -0700

    feat: variant support, variant order support, updated class list, tailwind generation

--- src/main/java/TailwindSorter.java
+++ src/main/java/TailwindSorter.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -5,15 +6,43 @@ public class TailwindSorter implements Comparator<String> {
     private final List<String> classOrder;
     private final int lastPosition;
 
-    public TailwindSorter(List<String> classOrder) {
+    private final List<String> variantOrder = Arrays.asList(
+            "first",
+            "last",
+            "odd",
+            "even",
+            "visited",
+            "checked",
+            "empty",
+            "read-only",
+            "group-hover",
+            "group-focus",
+            "focus-within",
+            "hover",
+            "focus",
+            "focus-visible",
+            "active",
+            "disabled",
+            "xs",
+            "sm",
+            "md",
+            "lg",
+            "xl",
+            "2xl",
+            "3xl"
+    );
+
+    public TailwindSorter(List<String> classOrder)
+    {
         this.classOrder = classOrder;
         this.lastPosition = classOrder.size();
     }
 
     @Override
-    public int compare(String s, String t1) {
-        int positionS = classOrder.indexOf(s);
-        int positionT1 = classOrder.indexOf(t1);
+    public int compare(String s, String t1)
+    {
+        int positionS = classOrder.indexOf(removeVariant(s)) + offsetVariant(s);
+        int positionT1 = classOrder.indexOf(removeVariant(t1)) + offsetVariant(t1);
         if(positionS == -1) {
             positionS = lastPosition;
         }
@@ -21,5 +50,35 @@ public class TailwindSorter implements Comparator<String> {
             positionT1 = lastPosition;
         }
         return positionS - positionT1;
+    }
+
+    /**
+     * Returns true if a variant is detected within this string
+     *
+     * @param className string class to be checked
+     *
+     * @return if a variant was detected at the start of the string, returns true
+     */
+    public int offsetVariant(String className) {
+        if(className.contains("dark:")) {
+            className = className.replace("dark:", "");
+        }
+
+        // If there is still a variant here, pull out the variant and see what natural order it shoudl be included in
+        return variantOrder.indexOf(className.substring(0, className.lastIndexOf(":")));
+    }
+
+    /**
+     * Removes the variant to check the original class location in the output
+     *
+     * @return string
+     */
+    public String removeVariant(String originalClassName)
+    {
+        int trimVariantAt = originalClassName.lastIndexOf(":");
+        if(trimVariantAt == -1) {
+            return originalClassName;
+        }
+        return originalClassName.substring(trimVariantAt);
     }
 }
