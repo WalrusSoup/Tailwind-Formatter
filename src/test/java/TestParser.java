db5ec633ca9bbf6b8c52429f1e7d5fba856619aa
JL <jay.lindsley@gmail.com>
2022-09-07 08:34:15 -0700

    Release: Version 1.5 (#54)
    * feat: adding support for tailwind 3.0 classes
    
    * feat: variant support, variant order support, updated class list, tailwind generation
    
    * feat: sorting by tailwind convention, fixing tests to sort by tailwind variant order
    
    Updates tests to reflect tailwind sorting as done by TW internally, & as documented.
    
    * feat: updating build process, gradle,
    
    * feat: new user defined configuration support via .tailwindorder file. Updated tests.
    
    * docs: updating readme file
    
    * docs: readme, changelog
    
    * feat: support for variant + dependent ordering
    
    Co-authored-by: Lance <lancet@myoutdesk.com>

--- src/test/java/TestParser.java
+++ src/test/java/TestParser.java
@@ -9,33 +9,78 @@ import java.util.Arrays;
 import java.util.List;
 
 public class TestParser {
-    private final List<String> classOrder = Arrays.asList(
+    private final List<String> customClassOrder = Arrays.asList(
         "first",
         "sm:first",
-        "sec_ond",
+        "second",
         "THIRD",
-        "lg:hover:last",
-        "group-focus:la-st",
+        "fourth",
+        "group-focus:fifth",
         "last"
     );
 
+    private final List<String> cssClassOrder = Arrays.asList(
+        "first",
+        "sec_ond",
+        "THIRD",
+        "group-focus:la-st",
+        "lg:hover:last",
+        "last",
+        "sm:first"
+    );
+
     @Test
-    public void testProcessBody() {
-        TailwindParser parser = new TailwindParser(new TailwindSorter(this.classOrder));
+    public void testProcessingCustomConfigurationOnCss()
+    {
+        TailwindParser parser = new TailwindParser(new TailwindSorter(cssClassOrder, true));
+        String input = "";
+        String expected = "";
 
-        Arrays.asList("html", "css").forEach(ext -> {
-            String input = "";
-            String expected = "";
+        try {
+            input = readFixtureFile("input.css");
+            expected = readFixtureFile("expected.css");
+        } catch (IOException | URISyntaxException e) {
+            Assert.fail("Could not load fixture file: " + e);
+        }
 
-            try {
-                input = readFixtureFile("input." + ext);
-                expected = readFixtureFile("expected." + ext);
-            } catch (IOException | URISyntaxException e) {
-                Assert.fail("Could not load fixture file: " + e);
-            }
+        Assert.assertEquals(expected, parser.processBody(input));
+    }
 
-            Assert.assertEquals(expected, parser.processBody(input));
-        });
+    @Test
+    public void testProcessingCustomConfigurationOnRegularFile()
+    {
+        TailwindParser parser = new TailwindParser(new TailwindSorter(customClassOrder, true));
+        String input = "";
+        String expected = "";
+
+        try {
+            input = readFixtureFile("custom.html");
+            expected = readFixtureFile("custom_expected.html");
+        } catch (IOException | URISyntaxException e) {
+            Assert.fail("Could not load fixture file: " + e);
+        }
+
+        Assert.assertEquals(expected, parser.processBody(input));
+    }
+
+    @Test
+    public void testProcessingVue()
+    {
+        TailwindUtility tailwindUtility = new TailwindUtility();
+        tailwindUtility.loadDefaultClassOrder();
+
+        TailwindParser parser = new TailwindParser(new TailwindSorter(tailwindUtility.classOrder, false));
+        String input = "";
+        String expected = "";
+
+        try {
+            input = readFixtureFile("input.vue");
+            expected = readFixtureFile("expected.vue");
+        } catch (IOException | URISyntaxException e) {
+            Assert.fail("Could not load fixture file: " + e);
+        }
+
+        Assert.assertEquals(expected, parser.processBody(input));
     }
 
     private String readFixtureFile(String file) throws IOException, URISyntaxException {
