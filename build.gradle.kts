e64a2a281d3a70c8300895caa5a5b00eb6cc505e
JL <jay.lindsley@gmail.com>
2023-04-07 08:49:55 -0700

    release: adding beta, eap release channel branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,6 +108,6 @@ tasks {
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+        channels.set(listOf("beta", "eap"))
     }
 }
