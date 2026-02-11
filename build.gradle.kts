9f4fcaf06db846f802d32300677e600247b623f9
JL <jay.lindsley@gmail.com>
2026-02-10 21:20:51 -0800

    feat: gradle updates. (#109)
    * feat: gradle updates.
    
    * wip: removing IC, updating build.
    
    * fix: only using latest IDE.
    
    * fix: build? maybe?
    
    * fix: deprecated IC->IU

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
-        create(providers.gradleProperty("platformType"), providers.gradleProperty("platformVersion"))
+        intellijIdea(providers.gradleProperty("platformVersion"))
 
         // Plugin Dependencies. Uses `platformBundledPlugins` property from the gradle.properties file for bundled IntelliJ Platform plugins.
         bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
@@ -102,7 +102,7 @@ intellijPlatform {
 
     pluginVerification {
         ides {
-            recommended()
+            ide(providers.gradleProperty("platformVersion").map { "IU-$it" })
         }
     }
 }
